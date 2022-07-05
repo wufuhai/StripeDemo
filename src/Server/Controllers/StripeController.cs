55e11564742a3b53dc62760f80c543cc0b5d502b
Kelvin Wu <wufuhai@qne.com.my>
2022-07-05 09:53:01 +0800

    step 6

--- src/Server/Controllers/StripeController.cs
+++ src/Server/Controllers/StripeController.cs
@@ -2,7 +2,10 @@
 using QNE.App.Server.Controllers;
 using Stripe;
 using StripeDemo.Shared;
+using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace StripeDemo.Server.Controllers
 {
@@ -66,9 +69,65 @@ namespace StripeDemo.Server.Controllers
                 var session = service.Create(options);
                 return this.Success("Get stripe billing success.", new ManageBillingResultModel { RedirectUrl = session.Url });
             }
-            catch(StripeException ex)
+            catch (StripeException ex)
             {
                 return this.Error("ERR_STRIPE_EX", ex.Message);
+            }
+        }
+
+        [HttpPost("webhook/[action]")]
+        public async Task<IActionResult> OnSubscriptionChanged()
+        {
+            var json = await new StreamReader(HttpContext.Request.Body).ReadToEndAsync();
+            // Replace this endpoint secret with your endpoint's unique secret
+            // If you are testing with the CLI, find the secret by running 'stripe listen'
+            // If you are using an endpoint defined with the API or dashboard, look in your webhook settings
+            // at https://dashboard.stripe.com/webhooks
+            const string endpointSecret = "whsec_d8c62633acb7534410891b32da63757a6bee4fd245f61794da14fd425cdcac4e";
+            try
+            {
+                var stripeEvent = EventUtility.ParseEvent(json, throwOnApiVersionMismatch: false);
+                var signatureHeader = Request.Headers["Stripe-Signature"];
+                stripeEvent = EventUtility.ConstructEvent(json,
+                        signatureHeader, endpointSecret, throwOnApiVersionMismatch: false);
+                if (stripeEvent.Type == Events.CustomerSubscriptionDeleted)
+                {
+                    var subscription = stripeEvent.Data.Object as Subscription;
+                    Console.WriteLine("A subscription was canceled.", subscription.Id);
+                    // Then define and call a method to handle the successful payment intent.
+                    // handleSubscriptionCanceled(subscription);
+                }
+                else if (stripeEvent.Type == Events.CustomerSubscriptionUpdated)
+                {
+                    var subscription = stripeEvent.Data.Object as Subscription;
+                    Console.WriteLine("A subscription was updated.", subscription.Id);
+                    // Then define and call a method to handle the successful payment intent.
+                    // handleSubscriptionUpdated(subscription);
+                }
+                else if (stripeEvent.Type == Events.CustomerSubscriptionCreated)
+                {
+                    var subscription = stripeEvent.Data.Object as Subscription;
+                    Console.WriteLine("A subscription was created.", subscription.Id);
+                    // Then define and call a method to handle the successful payment intent.
+                    // handleSubscriptionUpdated(subscription);
+                }
+                else if (stripeEvent.Type == Events.CustomerSubscriptionTrialWillEnd)
+                {
+                    var subscription = stripeEvent.Data.Object as Subscription;
+                    Console.WriteLine("A subscription trial will end", subscription.Id);
+                    // Then define and call a method to handle the successful payment intent.
+                    // handleSubscriptionUpdated(subscription);
+                }
+                else
+                {
+                    Console.WriteLine("Unhandled event type: {0}", stripeEvent.Type);
+                }
+                return Ok();
+            }
+            catch (StripeException e)
+            {
+                Console.WriteLine("Error: {0}", e.Message);
+                return BadRequest();
             }
         }
     }
